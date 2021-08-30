deff17b5bcf17ad8574e2cf166d2a7233de24c8a
sam <samantha.alvear18alv@gmail.com>
2021-08-30 18:15:31 -0500

    login

---
+++ login/login.php
@@ -0,0 +1,60 @@
+<?php
+session_reset();
+
+require_once '../database.php';
+
+if (!empty($_POST['email']) && !empty($_POST['password'])){
+$consulta = $conn->prepare('SELECT id, email, passwor FROM users  WHERE email = :email');
+$consukta->bindParam(':email',$_POST['email']);
+$consulta->execute();
+$resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+ 
+$message = '';
+
+if(count($resultado) > 0 && password_verify($_POST['password'], $resultado['passwor'])){
+    $_SESSION['user_id'] = $resultado['id'];
+    header('Location: /index.html');
+}else{
+    $message = 'usuario incorrecto';
+}
+
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>login</title>
+    <link rel = "preconnect" href = "https://fonts.gstatic.com" crossorigin>
+<link rel = "preconnect" href = "https://fonts.googleapis.com">
+<link rel = "preconnect" href = "https://fonts.gstatic.com" crossorigin>
+<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+
+
+
+<h1>Login</h1>
+
+   <form action="login.php" methood="post">
+      <input type="email" id="email" name="email" placeholder="usuario">
+      <input type="password" id="password" name="password" placeholder="password">
+      <input type="submit" name="submit" value="Enviar">
+    </form>
+    <a href="signup.php">REGISTRARSE</a>
+
+    <?php if(!empty($message)) : ?>
+
+        <h1><?= $message ?> </h1>
+     <?php endif;?>   
+
+</body>
+</html>
+
+
+
+
